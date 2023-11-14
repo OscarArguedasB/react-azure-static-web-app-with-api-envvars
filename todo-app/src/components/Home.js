cc8517f29db4e350243fba80e14b093ed0567e50
OscarArguedasB <78943877+OscarArguedasB@users.noreply.github.com>
2023-11-14 13:44:01 -0600

    Update Home.js

--- todo-app/src/components/Home.js
+++ todo-app/src/components/Home.js
@@ -4,59 +4,26 @@ import '../App.css';
 import Header from './Header'
 import { Tasks } from './Tasks'
 import CreateTask from './CreateTask'
-import { getAllTasks, deleteTask, fetchSettings } from '../services/TodoService'
+//import { getAllTasks, deleteTask, fetchSettings } from '../services/TodoService'
 
 function Home() {
 
-  const [tasks, setTasks] = useState([])
-  const [numberOfTasks, setNumberOfTasks] = useState([])
-  const [isTaskEdited, setTaskEdited] = useState(false)
-  const [appSettings, setAppSettings] = useState({});
+ // const [tasks, setTasks] = useState([])
+  //const [numberOfTasks, setNumberOfTasks] = useState([])
+  //const [isTaskEdited, setTaskEdited] = useState(false)
+  //const [appSettings, setAppSettings] = useState({});
 
-  useEffect(() => {
-    getAllTasks().then(tasks => {
-        console.log(tasks)
-        setTasks(tasks)
-      });
-  }, [numberOfTasks, isTaskEdited])
-
-  useEffect( () => {
-    async function fetchData() {
-      const response = await fetchSettings();
-    
-      setAppSettings(response);
-    }
-    fetchData();
-    
-  },[])
-
-  function delTask(taskId) {
-      deleteTask(taskId).then(response => {
-        console.log(response)
-        setNumberOfTasks(numberOfTasks - 1)
-      });
-  }
-
-  function taskCreated() {
-    setNumberOfTasks(numberOfTasks + 1)
-  }
-
-  function taskEdited(res) {
-     setTaskEdited(res)
-  }
     
   return (
     <div className="App">
-      <Header appSettings={appSettings}></Header>
+      <Header></Header>
       <div className="container mrgnbtm">
         <div className="row">
-          <div className="col-md-12">
-              <CreateTask taskCreated={taskCreated}></CreateTask>
-          </div>
+          
         </div>
       </div>
       <div className="row mrgnbtm">
-        <Tasks tasks={tasks} deleteTask={delTask} taskEdited={taskEdited}></Tasks>
+        
       </div>
     </div>
   );
