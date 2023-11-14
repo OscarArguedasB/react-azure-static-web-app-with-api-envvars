25c5a810548a2cf11da7bf0204f6fc89bf0061c6
OscarArguedasB <78943877+OscarArguedasB@users.noreply.github.com>
2023-11-14 14:37:57 -0600

    Update Home.js

--- todo-app/src/components/Home.js
+++ todo-app/src/components/Home.js
@@ -12,7 +12,7 @@ function Home() {
       <div className="container mrgnbtm">
         <div className="row">
           <div className="col-md-12">
-              <p>Hola</p>
+              <p>Hola 2</p>
           </div>
         </div>
       </div>
