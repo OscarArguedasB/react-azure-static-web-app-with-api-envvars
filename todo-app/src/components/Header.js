7aa38c4023b964767c6411d7ae37cb25a552b409
OscarArguedasB <78943877+OscarArguedasB@users.noreply.github.com>
2023-11-14 13:29:59 -0600

    Update Header.js

--- todo-app/src/components/Header.js
+++ todo-app/src/components/Header.js
@@ -1,22 +1,22 @@
+// In your React component
 import React from 'react';
 
-const Header = ({appSettings}) => {
-    const headerColor = appSettings?.headerColor || process.env.REACT_APP_HEADER_COLOR;
-    const headerTitleColor = appSettings?.headerTitleColor || process.env.REACT_APP_HEADER_TITLE_COLOR;
+const Header = () => {
+    // Accessing environment variables
+    const headerColor = process.env.REACT_APP_HEADER_COLOR;
+    const headerTitleColor = process.env.REACT_APP_HEADER_TITLE_COLOR;
 
-    console.log("headerColor", process.env.REACT_APP_HEADER_COLOR)
-    const { headerColor, headerTitleColor } = appSettings
     if (!headerColor) {
         return null;
     }
 
     return (
-        <div style={{backgroundColor: headerColor}} className="header">
-            <h1 style={{color: headerTitleColor}}>
+        <div style={{ backgroundColor: headerColor }} className="header">
+            <h1 style={{ color: headerTitleColor }}>
                 Azure Static Web Apps With React Demo
             </h1>
         </div>
-    )
+    );
 }
 
-export default Header
+export default Header;
