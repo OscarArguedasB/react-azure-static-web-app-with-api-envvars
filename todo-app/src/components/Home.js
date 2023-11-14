249f2fad3670ba5703e3b8889d3a10627c3e33f3
OscarArguedasB <78943877+OscarArguedasB@users.noreply.github.com>
2023-11-14 13:58:21 -0600

    Update Home.js

--- todo-app/src/components/Home.js
+++ todo-app/src/components/Home.js
@@ -1,26 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../App.css';
 import Header from './Header'
-import { Tasks } from './Tasks'
-import CreateTask from './CreateTask'
 //import { getAllTasks, deleteTask, fetchSettings } from '../services/TodoService'
 
 function Home() {
-
-  const [tasks, setTasks] = useState([])
-  const [numberOfTasks, setNumberOfTasks] = useState([])
-  const [isTaskEdited, setTaskEdited] = useState(false)
-  const [appSettings, setAppSettings] = useState({});
-   
-
-  function taskCreated() {
-    setNumberOfTasks(numberOfTasks + 1)
-  }
-
-  function taskEdited(res) {
-     setTaskEdited(res)
-  }
     
   return (
     <div className="App">
@@ -28,7 +12,7 @@ function Home() {
       <div className="container mrgnbtm">
         <div className="row">
           <div className="col-md-12">
-              <CreateTask taskCreated={taskCreated}></CreateTask>
+              <p>Hola<p>
           </div>
         </div>
       </div>
